31c47cb7af2ea8e412fdf79a0e16087b827aed16
Aditya4769 <114800003+Aditya4769@users.noreply.github.com>
2022-12-01 22:55:38 +0530

    PYTHON

---
+++ Chapter3-the__in-keyword-CipherSchools.py
@@ -0,0 +1,7 @@
+name = "Aditya"
+#  in keyword
+#  if with in 
+if  "h" in "Aditya":
+    print("h is present in the name")
+else:
+    print("not present")
---
+++ Chapter3-while_loop-CipherSchools.py
@@ -0,0 +1,7 @@
+#  loop
+#  while loop, for loop
+print("hello world") # 10 times
+i = 1
+while i<=10:
+    print(f"Aditya {i}")
+    i = i  + 1
